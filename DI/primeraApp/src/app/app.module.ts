1b232be15bee23b2bef857303a20717ab6c92851
Antonio Joaquín Montero García <montero.gaant20@aulas.triana>
2020-12-04 14:27:11 +0100

    antes del puente

---
+++ DI/primeraApp/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TableDataComponent } from './table-data/table-data.component';
+import {MatTableModule} from '@angular/material/table';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TableDataComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
